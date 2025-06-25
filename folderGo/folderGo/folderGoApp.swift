903dede8be853851997cbc9b469bc3a9e97b5168
JAEHYUN GO <gojaehyeonstudio@gmail.com>
2025-06-25 09:54:38 +0900

    doc: 프로젝트 기본 세팅

---
+++ folderGo/folderGo/folderGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  folderGoApp.swift
+//  folderGo
+//
+//  Created by Gojaehyun on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct folderGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ folderGo/folderGoTests/folderGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  folderGoTests.swift
+//  folderGoTests
+//
+//  Created by Gojaehyun on 6/25/25.
+//
+
+import Testing
+@testable import folderGo
+
+struct folderGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
